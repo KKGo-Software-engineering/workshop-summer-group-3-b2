c5a2fda7c5dc518a7b9748adf4aa895a80d391bf
Puttipong <puttipong.tangjanyatam@scbtechx.io>
2024-05-18 15:55:21 +0700

    add struct

---
+++ api/expense/expense.go
@@ -0,0 +1,13 @@
+package expense
+
+import "time"
+
+type Expense struct {
+	Id        int       `json:"id"`
+	Date      time.Time `json:"date"`
+	Amount    float64   `json:"amount"`
+	Category  string    `json:"category"`
+	Note      string    `json:"note"`
+	ImageUrl  string    `json:"image_url"`
+	SpenderId int       `json:"spender_id"`
+}
